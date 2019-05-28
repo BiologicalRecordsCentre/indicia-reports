f624341437f4ccf326f5f32dc712961e2add8018
John van Breda <john.vanbreda@biodiverseit.co.uk>
2019-05-28 10:53:38 +0100

    Index for UKSI search optimisation.

---
+++ db/version_2_26_0/201905571010_indices.sql
@@ -0,0 +1,6 @@
+-- Optimise the most common searches on UKSI on iRecord.
+CREATE INDEX ix_cache_taxon_searchterms_fulltext_filtered
+  ON cache_taxon_searchterms
+  USING gin
+  (to_tsvector('simple'::regconfig, quote_literal(quote_literal(original::text))))
+  WHERE simplified = false AND (language_iso<>'lat' or preferred=true) AND name_type<>'A' AND taxon_list_id=15
